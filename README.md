ea4546eb43449cdc70cdd16182ce88c7326ec689
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2025-01-29 12:24:42 +0100

    Init plugin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# extending-block-bindings
+# Extending block bindings
+
+This is an experimental plugin to see how block bindings could be extended without modifying the code in WordPress core.
---
+++ ebb.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       Extending Block Bindings
+ * Version:           0.1.0
+ * Requires at least: 6.7
+ * Requires PHP:      7.4
+ * Description:       Experimental plugin that extend core block bindings API.
+ * Author:            Automattic
+ * License:           GPL-3.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       ebb
+ */
