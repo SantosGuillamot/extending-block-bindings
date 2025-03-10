293eb6b1ef09ba19b451b526f549da0db88690c4
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2025-03-10 12:22:22 +0100

    Register fields for testing

---
+++ lib/register-fields.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Register testing custom fields for development.
+ */
+register_meta(
+	'post',
+	'text_custom_field',
+	array(
+		'show_in_rest'      => true,
+		'single'            => true,
+		'type'              => 'string',
+		'default'           => 'Text field value',
+		'revisions_enabled' => true,
+	)
+);
+register_meta(
+	'post',
+	'url_custom_field',
+	array(
+		'show_in_rest' => true,
+		'single'       => true,
+		'type'         => 'string',
+		'default'      => 'https://wpmovies.dev/wp-content/uploads/2023/04/goncharov-poster-original-1-682x1024.jpeg',
+	)
+);
